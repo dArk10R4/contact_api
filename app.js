014b9b77536d3680591f60c59256324197bb09bc
dArk10R4 <goylerov04@gmail.com>
2023-12-23 17:41:59 +0400

    add timestamps

--- app.js
+++ app.js
@@ -23,9 +23,22 @@ const messageSchema = mongoose.Schema({
   name: String,
   email: { type: String, required: true },
   message: String,
+  phone: String,
+  subject: String,
+}, {
+  timestamps: true
 });
 
 const Message = mongoose.model("Message", messageSchema);
+
+const newsSchema = mongoose.Schema({
+  email: { type: String, required: true },
+}, {
+  timestamps: true
+});
+
+const News = mongoose.model("News", newsSchema);
+
 const rateLimitMiddleware = setRateLimit({
     windowMs: 60 * 1000,
     max: 5,
@@ -38,10 +51,10 @@ const rateLimitMiddleware = setRateLimit({
 /**
  * middvares
  */
-// app.set('trust proxy', 1)
+app.set('trust proxy', 3)
 app.use(cors())
 app.use(helmet())
-// app.use(rateLimitMiddleware)
+app.use(rateLimitMiddleware)
 app.use(express.json());
 
 
@@ -66,12 +79,17 @@ function isEmail(str) {
   return emailRegex.test(str);
 }
 
+function isPhone(str) {
+  const phoneRegex = /^[+]*[(]{0,1}[0-9]{1,3}[)]{0,1}[-\s\./0-9]*$/g;
+  return phoneRegex.test(str);
+}
+
 
 /**
  * app routes
  */
 app.post("/contact", async (req, res) => {
-  const { name, email, message } = req.body;
+  const { name, email, message, phone, subject } = req.body.data;
   console.log(req.body)
   if (name) {
     if (!(isString(name)))
@@ -90,14 +108,55 @@ app.post("/contact", async (req, res) => {
   }
   if (message && !(isString(message)))
     return res.status(400).json({ error: "message must be a string" });
+  if (phone) {
+    if (!(isString(phone)))
+      return res.status(400).json({ error: "phone must be a string" });
+    if (!isPhone(phone))
+      return res.status(400).json({ error: "invalid phone format" });
+  }
   const new_message = new Message({
     name,
     email,
     message,
+    phone,
+    subject,
   });
 
   try {
     await new_message.save();
+    console.log(`inserted with email : ${email}`)
+    res.status(201).send({});
+  } catch (e) {
+    if (e.name === "ValidationError") {
+      let errors = {};
+
+      Object.keys(e.errors).forEach((key) => {
+        errors[key] = e.errors[key].message;
+      });
+      return res.status(400).send({"error":errors});
+    }
+
+    console.log(e);
+    return res.status(504).send({"error":"something get bad"});
+  }
+});
+
+app.post("/news", async (req, res) => {
+  const { email } = req.body.data;
+  console.log(req.body)
+  if (email) {
+    if (!(isString(email)))
+      return res.status(400).json({ error: "email must be a string" });
+    if (!isEmail(email))
+      return res.status(400).json({ error: "invalid email format" });
+  }
+
+  const new_news = new News({
+    email,
+  });
+
+  try {
+    await new_news.save();
     console.log(`inserted with email : ${email}`)
     res.status(201).send({});
   } catch (e) {
